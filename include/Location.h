1c86942b7e66bf3788e1b1e41bc3705cfefb6e28
BerkKoksal <151423150+BerkKoksal@users.noreply.github.com>
2024-10-21 20:27:56 -0400

    Update Location.h

---
+++ include/Location.h
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Location{
+    public:
+
+    string name;
+    string firstDescription;
+
+    void display();
+    void displayExits();
+
+    void addExit();
+    void addItems();
+     
+};
